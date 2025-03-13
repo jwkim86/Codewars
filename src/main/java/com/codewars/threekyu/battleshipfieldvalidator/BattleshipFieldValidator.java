5f2853192d86b057a91ef7b808c9550e0ee6618f
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-03-14 01:36:35 +0900

    Add Battleship field validator solution

---
+++ src/main/java/com/codewars/threekyu/battleshipfieldvalidator/BattleshipFieldValidator.java
@@ -0,0 +1,53 @@
+package com.codewars.threekyu.battleshipfieldvalidator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BattleshipFieldValidator {
+
+    public static boolean fieldValidator(int[][] field) {
+        Map<Integer, Integer> ships = new HashMap<>();
+        for(int i=0; i<10; i++) {
+            for(int j=0; j<10; j++) {
+                // Check if diagonal cells have number. This is MAIN IDEA!!
+                if(i<9 && j<9 && hasDiagonalNumber(field, i, j)) return false;
+
+                // If not empty cell
+                if(field[i][j] > 0) {
+
+                    // Check if next y cell is filled
+                    if(i<9 && field[i+1][j] == 1) {
+                        // Fill next y cell with current ship size +1
+                        field[i+1][j] = field[i][j]+1;
+                        // Check if next y cell ship size over 4
+                        if(field[i+1][j] > 4) return false;
+
+                    // Check if next x cell is filled
+                    } else if(j<9 && field[i][j+1] == 1) {
+                        // Fill next x cell with current ship size +1
+                        field[i][j+1] = field[i][j]+1;
+                        // Check if next x cell ship size over 4
+                        if(field[i][j+1] > 4) return false;
+
+                    // Every next cells are empty
+                    } else {
+                        // Ship size is confirmed, Add ship count
+                        ships.put(field[i][j], ships.getOrDefault(field[i][j], 0)+1);
+                    }
+                }
+            }
+        }
+        // Check ship count by ship size
+        if(ships.getOrDefault(4, 0) != 1) return false;
+        if(ships.getOrDefault(3, 0) != 2) return false;
+        if(ships.getOrDefault(2, 0) != 3) return false;
+        if(ships.getOrDefault(1, 0) != 4) return false;
+        return true;
+    }
+
+    public static boolean hasDiagonalNumber(int[][] field, int y, int x) {
+        if(field[y][x] > 0 && field[y+1][x+1] > 0) return true;
+        if(field[y+1][x] > 0 && field[y][x+1] > 0) return true;
+        return false;
+    }
+}
---
+++ src/test/java/com/codewars/threekyu/battleshipfieldvalidator/BattleshipFieldValidatorTest.java
@@ -0,0 +1,26 @@
+package com.codewars.threekyu.battleshipfieldvalidator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BattleshipFieldValidatorTest {
+
+
+    private static int[][] battleField = {
+            {1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+            {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+            {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+    @Test
+    public void SampleTest() {
+        assertEquals(true, BattleshipFieldValidator.fieldValidator(battleField));
+    }
+}
