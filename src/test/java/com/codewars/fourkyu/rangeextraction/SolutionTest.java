ca38e53a4de06347173ecf355ce4c184176c8e28
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-03-13 16:43:33 +0900

    Apply parameterized test

--- src/test/java/com/codewars/fourkyu/rangeextraction/SolutionTest.java
+++ src/test/java/com/codewars/fourkyu/rangeextraction/SolutionTest.java
@@ -1,32 +1,40 @@
 package com.codewars.fourkyu.rangeextraction;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
 
-    @Test
-    public void test1() {
-        assertEquals("-6,-3-1,3-5,7-11,14,15,17-20", Solution.rangeExtraction(new int[] {-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20}));
-        assertEquals("-3--1,2,10,15,16,18-20", Solution.rangeExtraction(new int[] {-3,-2,-1,2,10,15,16,18,19,20}));
-        assertEquals("-3--1,2,10,15,16,20", Solution.rangeExtraction(new int[] {-3,-2,-1,2,10,15,16,20}));
-        assertEquals("-3--1,2,10,15,16,19,20", Solution.rangeExtraction(new int[] {-3,-2,-1,2,10,15,16,19,20}));
+    private static Stream<Arguments> provideTestValues() {
+        return Stream.of(
+                Arguments.of("-6,-3-1,3-5,7-11,14,15,17-20", new int[] {-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20}),
+                Arguments.of("-3--1,2,10,15,16,18-20", new int[] {-3,-2,-1,2,10,15,16,18,19,20}),
+                Arguments.of("-3--1,2,10,15,16,20", new int[] {-3,-2,-1,2,10,15,16,20}),
+                Arguments.of("-3--1,2,10,15,16,19,20", new int[] {-3,-2,-1,2,10,15,16,19,20})
+        );
     }
 
-    @Test
-    public void test2() {
-        assertEquals("-6,-3-1,3-5,7-11,14,15,17-20", Solution.rangeExtraction2(new int[] {-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20}));
-        assertEquals("-3--1,2,10,15,16,18-20", Solution.rangeExtraction2(new int[] {-3,-2,-1,2,10,15,16,18,19,20}));
-        assertEquals("-3--1,2,10,15,16,20", Solution.rangeExtraction2(new int[] {-3,-2,-1,2,10,15,16,20}));
-        assertEquals("-3--1,2,10,15,16,19,20", Solution.rangeExtraction2(new int[] {-3,-2,-1,2,10,15,16,19,20}));
+    @ParameterizedTest
+    @MethodSource("provideTestValues")
+    public void test1(String expected, int[] input) {
+        assertEquals(expected, Solution.rangeExtraction(input));
     }
 
-    @Test
-    public void test3() {
-        assertEquals("-6,-3-1,3-5,7-11,14,15,17-20", Solution.rangeExtraction3(new int[] {-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20}));
-        assertEquals("-3--1,2,10,15,16,18-20", Solution.rangeExtraction3(new int[] {-3,-2,-1,2,10,15,16,18,19,20}));
-        assertEquals("-3--1,2,10,15,16,20", Solution.rangeExtraction3(new int[] {-3,-2,-1,2,10,15,16,20}));
-        assertEquals("-3--1,2,10,15,16,19,20", Solution.rangeExtraction3(new int[] {-3,-2,-1,2,10,15,16,19,20}));
+    @ParameterizedTest
+    @MethodSource("provideTestValues")
+    public void test2(String expected, int[] input) {
+        assertEquals(expected, Solution.rangeExtraction2(input));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestValues")
+    public void test3(String expected, int[] input) {
+        assertEquals(expected, Solution.rangeExtraction3(input));
     }
 }
